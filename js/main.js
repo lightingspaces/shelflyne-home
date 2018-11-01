11454430bfe068febbb5be9f4ed84b5e0f58382e
rhettparkinson <rhett.parkinson@gmail.com>
2018-11-01 18:59:03 +1100

    Updates - Shelflyne | Home

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Detect when form-control inputs are not empty
+  $(".rp-contactForm .form-control").on("input", function() {
+    if ($(this).val()) {
+      $(this).addClass("hasValue");
+    } else {
+      $(this).removeClass("hasValue");
+    }
+  });
+
+
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+  });
+
+  var $menuLink = $(".menu a");
+  $menuLink.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+  });
+
+
+})(jQuery); // End of use strict
+
+
+
