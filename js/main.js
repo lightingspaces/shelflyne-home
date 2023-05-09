9f7cb5f5129589d2aca647d2093fda7b61203910
Rhett Parkinson <work@rhettp.com>
2023-05-09 15:04:57 +0700

    UI and layout updates

--- js/main.js
+++ js/main.js
@@ -1,33 +1,116 @@
-(function($) {
-  "use strict"; // Start of use strict
+"use strict"; // Start of use strict
 
-  $(window).on('load', function(){
-    $("#preloader").delay(2000).fadeOut("slow");
-  });
+window.addEventListener('load', function() {
+  var preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+});
 
-  // Detect when form-control inputs are not empty
-  $(".sh-contactForm .form-control").on("input", function() {
-    if ($(this).val()) {
-      $(this).addClass("hasValue");
-    } else {
-      $(this).removeClass("hasValue");
-    }
-  });
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(e) {
+  hamburger.classList.toggle("is-active");
+});
 
-
-  var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-  });
-
-  var $menuLink = $(".menu a");
-  $menuLink.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
+var menuLinks = document.querySelectorAll(".menu a");
+menuLinks.forEach(function(menuLink) {
+  menuLink.addEventListener("click", function(e) {
+    hamburger.classList.toggle("is-active");
     fullpage_api.setAllowScrolling(true);
   });
+});
 
+// Plyr
 
-})(jQuery); // End of use strict
+const player = new Plyr('#player', {
+  controls: [
+      'play-large', // The large play button in the center
+      'play', // Play/pause playback
+      'progress', // The progress bar and scrubber for playback and buffering
+      'volume', // Volume control
+      'fullscreen', // Toggle fullscreen
+  ],
+  fullscreen: {enabled: true, fallback: false, iosNative: true}
+});
 
+player.on('play', function() {
+        player.fullscreen.enter()
+    });
 
+// Fullpage
 
+var myFullpage = new fullpage('#fullpage', {
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  touchSensitivity: 5,
+  normalScrollElements: '#orderingCodes',
+  normalScrollElementTouchThreshold: 1,
+  scrollOverflow: true,
+  scrollingSpeed: 1500,
+  anchors: ['led-lighting', 'simple-to-install', 'premium-finish', 'modular-configuration', 'sizing-options', 'contact'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['Shelflyne Home', 'Simple to intall', 'Premium finish', 'Modular configuration', 'Sizing options', 'Talk to us'],
+  onLeave: function(origin, destination, direction){
+        var leavingSection = this;
+
+        //after leaving section 1
+        if(origin.index === 0){
+          let video = document.getElementById('video-1');
+          setTimeout(function() {
+                video.load();
+            }, 1500);
+        }
+        //after leaving section 2
+        if(origin.index === 1){
+          let video = document.getElementById('video-2');
+            setTimeout(function() {
+                video.load();
+            }, 1500);
+        }
+        //after leaving section 3
+        if(origin.index === 2){
+          let video = document.getElementById('video-3');
+            setTimeout(function() {
+                video.load();
+            }, 1500);
+        }
+        //after leaving section 4
+        if(origin.index === 3){
+          let video = document.getElementById('video-4');
+            setTimeout(function() {
+                video.load();
+            }, 1500);
+        }
+        //after leaving section 5
+        if(origin.index === 4){
+          let video = document.getElementById('video-5');
+            setTimeout(function() {
+                video.load();
+            }, 1500);
+        }
+      }
+});
+
+// Menu
+
+var slideout = new Slideout({
+  'panel': document.getElementById('main'),
+  'menu': document.getElementById('menu'),
+  'padding': 320,
+  'tolerance': 70,
+  'side': 'right',
+  'touch': false
+});
+
+document.querySelector('.menu').addEventListener('click', function(eve) {
+  if (eve.target.nodeName === 'A') { slideout.close(); }
+});
+
+document.querySelector('.slideout-toggle').addEventListener('click', function() {
+  slideout.toggle();
+  if(slideout.isOpen()){
+    fullpage_api.setAllowScrolling(false);
+    console.log('scroll false');
+  } else {
+    fullpage_api.setAllowScrolling(true);
+    console.log('scroll true');
+  }
+});
